2a7cb862d394c69e6a77b931cba07896ceecc1b1
Ray Lu <955226@hshs.ntpc.edu.tw>
2020-12-27 20:22:58 +0800

    Ray's lab if1.py

---
+++ EP32.py
@@ -0,0 +1,5 @@
+rain = input('Is it rain today: ')
+if rain == 'yes':
+    print('Go out with an umbrella')
+    print('Buy a pack of potato chips')
+    print('Go to the movies')
---
+++ if1.py
@@ -0,0 +1,4 @@
+age = input('What is your age? : ')
+age = int(age)
+if age >=20:
+    print('你可以投票')
